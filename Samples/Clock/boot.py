18283e319bfc344fd6207df4a29dc21e4866e950
vro256400 <vro256400@proton.me>
2026-01-18 13:59:00 +0200

    fix clock device

--- Samples/Clock/boot.py
+++ Samples/Clock/boot.py
@@ -7,12 +7,13 @@ class ClockApp(BoardApp.BoardApp):
     # it is info for web application
     def completeInputsVar(self, cnf):
         cnf = cnf + "ntp_domain : " + self.ntp.lastLoadedDomain + "\n"
-        cnf = cnf + "timezone : " + self.ntp.tz + "\n"
+        cnf = cnf + "timezone : " + str(self.ntp.tz) + "\n"
         a = time.time() + self.ntp.tz*60*60
         t = time.localtime(a)
-        cnf = cnf + "timeH : " + t[3] + "\n"
-        cnf = cnf + "timeM : " + t[4] + "\n"
+        cnf = cnf + "timeH : " + str(t[3]) + "\n"
+        cnf = cnf + "timeM : " + str(t[4]) + "\n"
         
+        print("cnf=" + cnf)
         return
 
 theApp = ClockApp()
